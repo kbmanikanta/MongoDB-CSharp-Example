443fa7e85e153d1c39a404182d874b52a54de286
jicarretero <jicarretero@wke.es>
2015-09-30 14:55:23 +0200

    mapper

---
+++ MongoDBExample/Mappers/ClientMapper.cs
@@ -0,0 +1,17 @@
+﻿using MongoDBExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDBExample.Mappers
+{
+    public class ClientMapper : IMapper<string, Client>
+    {
+        public Client Mapper(string origen)
+        {
+            return new Client(88, origen);
+        }
+    }
+}
