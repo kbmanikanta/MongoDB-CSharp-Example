9fe095b04d506de5d2c3468f3b5723ff0eb3bb69
narnau <arnau.julia@gmail.com>
2015-09-30 12:15:32 +0200

    comments added

--- MongoDBExample/Program.cs
+++ MongoDBExample/Program.cs
@@ -19,17 +19,23 @@ namespace MongoDBExample
             string databaseName = "test";
             string document = "poker";
 
+            //Connection
             var dbConnection = new MongoConnection();
             dbConnection.OpenConnection();
             IMongoDatabase mongoDatabase = dbConnection.GetDatabase(databaseName);
 
+            //Get Repository
             var mongoRepository = new MongoRepository(mongoDatabase, document);
 
 
+            //Queries
+
+            //First query: GetById
             var resultGetById = mongoRepository.GetById("1");
             Console.WriteLine(resultGetById.Result.ToList<BsonDocument>().ToJson());
             Console.ReadLine();
 
+            //Second query: GetFiltered
             IList<QueryInfo> queryGetFiltered = new List<QueryInfo>();
             queryGetFiltered.Add(new QueryInfo("_id", "1", "$eq"));
             queryGetFiltered.Add(new QueryInfo("name", "Prueba test", "$eq"));
