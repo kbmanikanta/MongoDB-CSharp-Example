03571f6ce505b2666d9474a8cbca1624ba6bd83a
narnau <arnau.julia@gmail.com>
2015-09-30 09:30:08 +0200

    mongoquery working

--- MongoDBExample/Queries/MongoQuery.cs
+++ MongoDBExample/Queries/MongoQuery.cs
@@ -8,14 +8,17 @@ using System.Threading.Tasks;
 
 namespace MongoDBExample.Queries
 {
-    public class MongoQuery : IQuery<Dictionary<string, string>, FilterDefinition<BsonDocument>>
+    public class MongoQuery : IQuery<Dictionary<string, string>, BsonDocument>
     {
-        public FilterDefinition<BsonDocument> CreateFilterQuery(Dictionary<string, string> filterParams)
+        public BsonDocument CreateFilterQuery(Dictionary<string, string> filterParams)
         {
-            var filter = Builders<BsonDocument>.Filter;
-            var filterDefinition =  filter.Eq("_id", "1") & filter.Eq("name", "Prueba test");
 
-            return filterDefinition;
+            var filterr = new BsonDocument("_id", new BsonDocument("$eq", "1"));
+            var filter2 = new BsonDocument("name", new BsonDocument("$eq", "Prueba test"));
+
+            filterr.AddRange(filter2);
+
+            return filterr;
         }
     }
 }
