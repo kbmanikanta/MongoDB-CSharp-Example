36d4c405ee3c73ba96134ff96a184f0936e2337a
narnau <arnau.julia@gmail.com>
2015-10-01 17:12:08 +0200

    SQLServer connection

---
+++ MongoDBExample/DBConnection/SQLServerConnection.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Data.SqlClient;
+
+namespace MongoDBExample.DBConnection
+{
+    public class SQLServerConnection : IDBConnection<SqlConnection>
+    {
+        private SqlConnection conn;
+
+        public SQLServerConnection()
+        {
+            this.conn = new System.Data.SqlClient.SqlConnection();
+            conn.ConnectionString = "Data Source=\\SQL2012;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        }
+
+        public bool CloseConnection()
+        {
+            try
+            {
+                conn.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return true;
+            
+        }
+
+        public SqlConnection GetDatabase(string databaseName)
+        {
+            return this.conn;
+        }
+
+        public bool OpenConnection()
+        {
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+            return true;
+        }
+    }
+}
---
+++ MongoDBExample/Repository/SQLServerRepository.cs
@@ -0,0 +1,42 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDBExample.Queries;
+using MongoDB.Driver;
+using MongoDBExample.Models;
+using MongoDBExample.Mappers;
+
+namespace MongoDBExample.Repository
+{
+    public class SQLServerRepository<TEntity, TDatabase> : IRepository<IEnumerable<BsonDocument>, string, IList<FilterQuery>, TEntity>
+    {
+        private IMongoDatabase sqlDataBase;
+        private string document;
+        private IMapper<TEntity, BsonDocument> mapper;
+
+        public SQLServerRepository(TDatabase sqlDataBase, string document, IMapper<TEntity, BsonDocument> mapper)
+        {
+            this.sqlDataBase = (IMongoDatabase)sqlDataBase;
+            this.document = document;
+            this.mapper = mapper;
+        }
+
+        public IEnumerable<BsonDocument> GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<BsonDocument> GetFiltered(IList<FilterQuery> query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Create(TEntity recurse)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
