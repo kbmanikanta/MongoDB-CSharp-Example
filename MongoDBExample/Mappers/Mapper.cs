88ecacf70bf1627b8e30714774ff694e208521f9
narnau <arnau.julia@gmail.com>
2015-10-01 15:53:42 +0200

    cleaning solution

---
+++ MongoDBExample/Mappers/Mapper.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDBExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDBExample.Mappers
+{
+    public class Mapper<T>
+    {
+        private IConverter<T> converter;
+
+        public Mapper(IConverter<T> converter)
+        {
+            this.converter = converter;
+        }
+
+        public BsonDocument ToBsonDocument(T client)
+        {
+            this.converter.ConvertToBsonDocument(client);
+            return new BsonDocument();
+        }
+    }
+}
